9f793386fdae24c84cffa53c32809767f89a4a4c
xujiliang <910940173@qq.com>
2017-08-26 18:43:25 +0800

    Hibernate

---
+++ Hibernate3/src/com/hibernate/subclass/HibernateTest.java
@@ -0,0 +1,81 @@
+package com.hibernate.subclass;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.Work;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hibernate.join.subclass.Person;
+import com.hibernate.join.subclass.Student;
+
+public class HibernateTest {
+
+	private SessionFactory sessionFactory;
+	private Session session;
+	private Transaction transaction;
+	
+	@Before
+	public void init(){
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = 
+				new ServiceRegistryBuilder().applySettings(configuration.getProperties())
+				                            .buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+	}
+	
+	@After
+	public void destroy(){
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+	
+	/**
+	 * 缺点:
+	 * 1. 使用了辨别者列.
+	 * 2. 子类独有的字段不能添加非空约束.
+	 * 3. 若继承层次较深, 则数据表的字段也会较多. 
+	 */
+	
+	/**
+	 * 查询:
+	 * 1. 查询父类记录, 只需要查询一张数据表
+	 * 2. 对于子类记录, 也只需要查询一张数据表
+	 */
+	
+	@Test
+	public void testSave(){
+		Person person = new Person();
+		person.setName("CC");
+        person.setAge(28);		
+        session.save(person);
+        Student student = new Student();
+        student.setName("DD");
+        student.setAge(30);
+        student.setSchool("dd");
+        session.save(student);
+       
+	
+	}
+
+}
